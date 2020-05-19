d6ad893ff905f108272e58fa104e48f782e855dc
Sarah Lafer <sarah_lafer@yahoo.com>
2020-05-19 11:58:01 +0200

    CRUD for restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/restaurants", to: "restaurants#index"
+  # get "/restaurants/new", to: "restaurants#new"
+  # post "/restaurants", to: "restaurants#create"
+  # get "/restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "/restaurants/:id", to: "restaurants#update"
+  # get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+  # delete "/restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 end
